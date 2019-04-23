cb64794a918c431b1e6d7f93cfde79e851e2133f
steekam <waynewanyee@gmail.com>
2019-04-23 23:27:38 +0300

    Complete desktop style of hero

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 <?php require_once INC_PATH.'header.php';?>
 
 <!-- Hero Section -->
-<section class="banner-4 parallax-bg bg-fixed overlay dark-5 fullscreen-banner valign-wrapper" data-stellar-background-ratio="0.5">
-	<div class="valign-cell">
-		<div class="container padding-top-110">
-			<div class="row">
-				<div class="col-md-7">
-					<h1 class="intro-title text-uppercase white-text mb-30" style="color: aquamarine !important;">Kuguru Food Complex Ltd.</h1>
-					<p class="lead text-regular white-text">Commitment to Excellence in Service and Quality products to Uplift and Empower Kenya and its People</p>
-				</div><!-- col-md-6 -->
-			</div><!-- row -->
+<section class="main-banner fullscreen-banner valign-wrapper" data-stellar-background-ratio="0.5">
+	<div class="d-flex justify-content-center align-items-center pt-100">
+		<div class="container d-flex justify-content-center">
+			<div class="text-center hero-text-wrapper">
+				<p class="white-text mb-3">Commitment to excellence in service and quality products to uplift and empower Kenya and its people</p>
+				<button class="btn btn-info btn-lg waves-effect waves-light">
+					Learn more
+				</button>
+			</div>
 		</div><!-- /.container -->
 	</div><!-- /.valign-cell -->
 </section>
