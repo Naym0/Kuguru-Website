d831d7f72305709554c8bc8ec7150ced03af5d82
Nicole <nicole.muswanya@strathmore.edu>
2019-04-27 18:00:43 +0300

    Improved and responsive landing page

--- index.php
+++ index.php
@@ -16,4 +16,83 @@
 </section>
 <!-- Hero Section End -->
 
+<section class="section mb-30">
+	<div class="container">
+
+		<div class="text-center mt-30">
+			<h2 class="section-title text-uppercase">Who are we?</h2>
+		</div>
+
+		<div class="row">
+			<div class="col-md-12">
+				<p>Kuguru Food Complex Limited was founded in 1988. It is one of the top manufactures of beverages in the Kenyan Republic. 
+					It is a family business founded by the family patriarch, Mr. Peter Kuguru. 
+					He and his wife built up the company for five years before being joined by four new family members. 
+					The most promising projects are the soft drink bottling plant and ready to drink fruit juices. 
+					This sector of the beverage market offers unlimited possibilities to KFCL's already established beverage network. 
+					The company has ensured that it maintains policies that are visionary and intrepid to stay ahead of the ever-dynamic Kenyan economy.</p>
+			</div><!-- /.col-md-12 -->
+		</div><!-- /.row -->
+
+		
+	</div><!-- /.container -->
+</section>
+
+<section class="section gray-bg">
+	<div class="container">
+
+		<div class="text-center mb-10 mt-30" id="about">
+			<h2 class="section-title text-uppercase">What we offer</h2>
+		</div>
+
+		<div class="row equal-height-row">
+
+			<div class="col-md-4 mb-30">
+			<div class="featured-item border-box radius-4 hover brand-hover equal-height-column">
+				<div class="icon mb-30">
+					<i class="material-icons brand-icon"><img src="assets/img/favicon.jpg"></i>
+				</div>
+				<div class="desc">
+					<h2>Bottling Drinks</h2>
+					<p>Softa Bottling Company Ltd (SBCL) manages several great brands. 
+					Softa and Babito soft drinks maintain international standards through technical backing from Dohler Euro Citrus in Germany.</p><br>
+					<a href="<?= BASE_URL?>/pages/softa.php">Read More -></a><br>
+				</div>
+			</div><!-- /.featured-item -->
+			</div><!-- /.col-md-4 -->
+
+
+			<div class="col-md-4 mb-30">
+			<div class="featured-item border-box radius-4 hover brand-hover equal-height-column">
+				<div class="icon mb-30">
+					<i class="material-icons brand-icon"><img src="assets/img/flour1.png"></i>
+				</div>
+				<div class="desc">
+					<h2>Milling Company</h2>
+					<p>The Cateress Milling Company (KFCL's milling company) produces high quality first grade sifted maize flour, otherwise known as "Cateress". 
+					Cateress milling uses both Swiss and Italian technology.</p><br>
+					<a href="<?= BASE_URL?>/pages/cateress.php">Read More -></a>
+				</div>
+			</div><!-- /.featured-item -->
+			</div><!-- /.col-md-4 -->
+
+
+			<div class="col-md-4 mb-30">
+			<div class="featured-item border-box radius-4 hover brand-hover equal-height-column">
+				<div class="icon mb-30">
+					<i class="material-icons brand-icon"><img src="assets/img/house1.png"></i>
+				</div>
+				<div class="desc">
+					<h2>Real Estate</h2>
+					<p>The company is also involved in minor real estate leasing and sale. In the branch of the company named Just Real.</p><br>
+					<a href="<?= BASE_URL?>/pages/contact.php">Read More -></a><br><br><br>
+				</div>
+			</div><!-- /.featured-item -->
+			</div><!-- /.col-md-4 -->
+						
+		</div><!-- /.row -->
+
+	</div><!-- /.container -->
+</section>
+
 <?php require_once INC_PATH.'footer.php';?>
