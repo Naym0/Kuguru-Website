a85d109e3fa90ebbef9f21024439311db56675f8
Nicole <nicole.muswanya@strathmore.edu>
2019-04-29 09:19:23 +0300

    About us page

--- pages/about.php
+++ pages/about.php
@@ -5,155 +5,111 @@
   $page_title = '';
 ?>
 <?php require_once INC_PATH.'header.php';?>
-  <section class="section-padding">
-      <div class="container">
-        <div class="text-center mb-80">
-            <h2 class="section-title">ABOUT US</h2>
-            <p class="section-sub"> ...... </p>
-        </div>
+<section class="section-padding">
+            <div class="container">
+              <div class="text-center mb-80">
+                  <h2 class="section-title">ABOUT US</h2>
+                  <!--<p class="section-sub"> ...... </p>-->
+              </div>
 
-        <div class="row latest-news-card">
-            <div class="col-xs-12 col-sm-6 col-md-4">
-              <article class="card">
-                <div class="card-image waves-effect waves-block waves-light">
-                  <img class="activator" src="<?= ASSETS_URL?>/img/blog/blog-18.jpg" alt="image">
-                </div>
-                <div class="card-content">
-                  <h2 class="entry-title activator">OUR MISSION</h2>
-                </div>
-                <div class="card-reveal overlay-blue">
-                  <span class="card-title close-button"><i class="fa fa-times"></i></span>
-                  <h2 class="entry-title"><a href="#">Material Design is best</a></h2>
-                  <p>Authoritatively grow quality technologies for strategic sources. Dramatically evolve front-end services quality.</p>
-                </div>
-              </article><!-- /.card -->
-            </div><!-- /.col-md-4 -->
+              <div class="row latest-news-card">
+                  <div class="col-xs-12 col-sm-6 col-md-4">
+                    <article class="card">
+                      <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="../assets/img/AboutUs/ourMission.jpg" alt="image">
+                      </div>
+                      <div class="card-content">
+                        <h2 class="entry-title activator">OUR MISSION</h2>
+                      </div>
+                      <div class="card-reveal overlay-blue">
+                        <span class="card-title close-button"><i class="fa fa-times"></i></span>
+                        <h2 class="entry-title"><a href="#">Mission Statement</a></h2>
+                        <p>
+                          Commitment to Excellence in Service and Quality products to Uplift and Empower Kenya and its People 
+                        </p>
+                      </div>
+                    </article><!-- /.card -->
+                  </div><!-- /.col-md-4 -->
 
-            <div class="col-xs-12 col-sm-6 col-md-4">
-              <article class="card">
-                <div class="card-image waves-effect waves-block waves-light">
-                  <img class="activator" src="<?= ASSETS_URL?>/img/blog/1.jpg" alt="image">
-                </div>
-                <div class="card-content">
-                  <h2 class="entry-title activator">PRODUCT MISSION</h2>
-                </div>
-                <div class="card-reveal overlay-blue">
-                  <span class="card-title close-button"><i class="fa fa-times"></i></span>
-                  <h2 class="entry-title"><a href="#">How To Be More Organized</a></h2>
-                  <p>Authoritatively grow quality technologies for strategic sources. Dramatically evolve front-end services quality.</p>
-                </div>
-              </article><!-- /.card -->
-            </div><!-- /.col-md-4 -->
+                  <div class="col-xs-12 col-sm-6 col-md-4">
+                    <article class="card">
+                      <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="../assets/img/AboutUs/products.jpg" alt="image">
+                      </div>
+                      <div class="card-content">
+                        <h2 class="entry-title activator">PRODUCT MISSION</h2>
+                      </div>
+                      <div class="card-reveal overlay-blue">
+                        <span class="card-title close-button"><i class="fa fa-times"></i></span>
+                        <h2 class="entry-title"><a href="#">PRODUCT MISSION</a></h2>
+                        <p>
+                          Commitment to the highest possible Standards in product Quality with the utmost Value in price
+                        </p>
+                      </div>
+                    </article><!-- /.card -->
+                  </div><!-- /.col-md-4 -->
 
-            <div class="col-xs-12 col-sm-6 col-md-4">
-              <article class="card">
-                <div class="card-image waves-effect waves-block waves-light">
-                  <img class="activator" src="<?= ASSETS_URL?>/img/blog/3.jpg" alt="image">
-                </div>
-                <div class="card-content">
-                  <h2 class="entry-title activator">STAKEHOLDERS MISSION</h2>
-                </div>
-                <div class="card-reveal overlay-blue">
-                  <span class="card-title close-button"><i class="fa fa-times"></i></span>
-                  <h2 class="entry-title"><a href="#">Exploring the Product Design7</a></h2>
-                  <p>Authoritatively grow quality technologies for strategic sources. Dramatically evolve front-end services quality.</p>S
-                </div>
-              </article><!-- /.card -->
-            </div><!-- /.col-md-4 -->
-        </div>
-      </div>
-  </section>
+                  <div class="col-xs-12 col-sm-6 col-md-4">
+                    <article class="card">
+                      <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="../assets/img/AboutUs/stakeholders.jpg" alt="image">
+                      </div>
+                      <div class="card-content">
+                        <h2 class="entry-title activator">STAKEHOLDERS MISSION</h2>
+                      </div>
+                      <div class="card-reveal overlay-blue">
+                        <span class="card-title close-button"><i class="fa fa-times"></i></span>
+                        <h2 class="entry-title"><a href="#">STAKEHOLDERS MISSION</a></h2>
+                        <p>
+                          Commitment to employee Satisfaction through educational training, work environment and compensation. Striving to maximize Freedom, Happiness, and human Dignity within the bounds of Profitability.
+                        </p>
+                      </div>
+                    </article><!-- /.card -->
+                  </div><!-- /.col-md-4 -->
+              </div>
+            </div>
+        </section>
 
-  <section class="section-padding banner-6 bg-fixed parallax-bg overlay light-9"data-stellar-background-ratio="0.5">
-      <div class="container">
+        <section class="section-padding banner-6 bg-fixed parallax-bg overlay light-9"data-stellar-background-ratio="0.5">
+            <div class="container">
 
-        <div class="text-center mb-50">
-            <h2 class="section-title">What We Do</h2>
-            <p class="section-sub">Quisque non erat mi. Etiam congue et augue sed tempus. Aenean sed ipsum luctus, scelerisque ipsum nec, iaculis justo. Sed at vestibulum purus, sit amet viverra diam nulla ac nisi rhoncus.</p>
-        </div>
+              <div class="text-center mb-50">
+                  <h2 class="section-title">What's New</h2>
+                  <p class="section-sub">
+                      Angelweiss Juices have launched several exciting new flavors! Look for our new flavors in your supermarket or kiosk today.
+                      Angelweiss long awaited concentrates have arrived to join the popular ready-to-drink product line.                   
+                      Softa wholesale containers continue to land all over Nairobi! Softa, Angelweiss, and Unga products are all available through those wholesale containers in selected parts of Nairobi. Watch for our promotions team whom will be visiting the container sites... you could win caps, pens, free drinks, not to mention unbelievable discounts for retailers. 
+                      Softa Soda has launched a new pushcart project placing pushcarts throughout the greater Nairobi area. It is easy to get involved and interested parties are asked to start with at least ten pushcarts. 
+                      KFCL estate division, in conjuction with Barclays Bank, is offering fully-serviced plots for sale. Financing Available!
+                  </p>
+              </div>
+        </section>
 
-        <div class="featured-carousel brand-dot">
-            <div class="featured-item border-box radius-4 hover brand-hover">
-                <div class="icon mb-30">
-                    <i class="material-icons brand-icon">&#xE32A;</i>
-                </div>
-                <div class="desc">
-                    <h2>Business Consulting</h2>
-                    <p>Porttitor communicate pandemic data rather than enabled niche pandemic data rather markets neque pulvinar vitae.</p>
-                </div>
-            </div><!-- /.featured-item -->
 
-            <div class="featured-item border-box radius-4 hover brand-hover">
-                <div class="icon mb-30">
-                    <i class="material-icons brand-icon">&#xE851;</i>
-                </div>
-                <div class="desc">
-                    <h2>Consumer Consulting</h2>
-                    <p>Porttitor communicate pandemic data rather than enabled niche pandemic data rather markets neque pulvinar vitae.</p>
-                </div>
-            </div><!-- /.featured-item -->
+        <section class="section-padding">
+            <div class="container">
 
-            <div class="featured-item border-box radius-4 hover brand-hover">
-                <div class="icon mb-30">
-                    <i class="material-icons brand-icon">&#xE8AF;</i>
-                </div>
-                <div class="desc">
-                    <h2>Financial Department</h2>
-                    <p>Porttitor communicate pandemic data rather than enabled niche pandemic data rather markets neque pulvinar vitae.</p>
-                </div>
-            </div><!-- /.featured-item -->
+              <h2 class="text-bold text-capitalize mb-30">We are serving since 1997</h2>
+              <div class="row">
+                <div class="col-md-7">                  
+                  <p>
+                      KFCL (Kuguru Food Complex Limited) has had experience in the mass-market beverage manufacture and distribution since 1992; KFCL was founded in 1988. KFCL is one of the top manufactures of beverages in the Kenyan Republic. KFCL is a family business founded by the family patriarch, Mr. Peter Kuguru. He and his wife built up the company for five years before being joined by four new family members.
+                      KFCL began manufacturing the product Chibuku, a South African Sorghum beer, originally operated by the multinational Lonrho in Kenya. The success of the alcoholic beverages did not stop KFCL from diversifying into mass-market products. KFCL expanded into maize meal starting with two mills followed by procuring plastic manufacturing machines that promoted quality through plastic packaged products.                       
+                      In an effort to diversify, KFCL decided to leave alcoholic beverages and concentrate solely on soft drinks. The most promising projects are the soft drink bottling plant and ready to drink fruit juices. This sector of the beverage market offers unlimited possibilities to KFCL's already established beverage network. KFCL has ensured that it maintains policies that are visionary and intrepid to stay ahead of the ever-dynamic Kenyan economy.
+                  </p>
+                </div><!-- /.col-md-7 -->
 
-            <div class="featured-item border-box radius-4 hover brand-hover">
-                <div class="icon mb-30">
-                    <i class="material-icons brand-icon">&#xE91D;</i>
-                </div>
-                <div class="desc">
-                    <h2>Travel & Transport</h2>
-                    <p>Porttitor communicate pandemic data rather than enabled niche pandemic data rather markets neque pulvinar vitae.</p>
-                </div>
-            </div><!-- /.featured-item -->
-
-            <div class="featured-item border-box radius-4 hover brand-hover">
-                <div class="icon mb-30">
-                    <i class="material-icons brand-icon">&#xE8CB;</i>
-                </div>
-                <div class="desc">
-                    <h2>Lawyer Cases</h2>
-                    <p>Porttitor communicate pandemic data rather than enabled niche pandemic data rather markets neque pulvinar vitae.</p>
-                </div>
-            </div><!-- /.featured-item -->
-
-            <div class="featured-item border-box radius-4 hover brand-hover">
-                <div class="icon mb-30">
-                    <i class="material-icons brand-icon">&#xE8DC;</i>
-                </div>
-                <div class="desc">
-                    <h2>Stocks And Trading</h2>
-                    <p>Porttitor communicate pandemic data rather than enabled niche pandemic data rather markets neque pulvinar vitae.</p>
-                </div>
-            </div><!-- /.featured-item -->
-
-            <div class="featured-item border-box radius-4 hover brand-hover">
-                <div class="icon mb-30">
-                    <i class="material-icons brand-icon">&#xE02C;</i>
-                </div>
-                <div class="desc">
-                    <h2>Business Strategies</h2>
-                    <p>Porttitor communicate pandemic data rather than enabled niche pandemic data rather markets neque pulvinar vitae.</p>
-                </div>
-            </div><!-- /.featured-item -->
-
-            <div class="featured-item border-box radius-4 hover brand-hover">
-                <div class="icon mb-30">
-                    <i class="material-icons brand-icon">&#xE0BE;</i>
-                </div>
-                <div class="desc">
-                    <h2>Business Solutions</h2>
-                    <p>Porttitor communicate pandemic data rather than enabled niche pandemic data rather markets neque pulvinar vitae.</p>
-                </div>
-            </div><!-- /.featured-item -->
-        </div>
-
-      </div><!-- /.container -->
-  </section>
+                <div class="col-md-5 mt-sm-50">
+                  <div class="gallery-thumb">
+                    <ul class="slides">
+                      <!--<li data-thumb="assets/img/blog/5.jpg">-->
+                        <img src="../assets/img/AboutUs/Kuguru.jpg" alt="image" id="kuguru">
+                      </li>
+                    </ul>
+                  </div>
+                  <!-- /.gallery-thumb -->
+                </div><!-- /.col-md-5 -->
+              </div><!-- /.row -->
+            </div><!-- /.container -->
+        </section>
 <?php require_once INC_PATH.'footer.php';?>
